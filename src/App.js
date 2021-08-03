54107d37ff92f8d175afda7b657fdcdbaa37fb9a
yashimwong <yashimwong@gmail.com>
2021-08-03 21:56:55 +0800

    feat: initial project config eslint prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Tabby"),
-    React.createElement("h3", {}, "Cat"),
-    React.createElement("h3", {}, "Common Short Hair"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
   ]);
 };
 
