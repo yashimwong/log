e066a7f743526fe7115388b0b8526745175096b0
yashimwong <yashimwong@gmail.com>
2021-08-04 00:38:47 +0800

    feat: add animal dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
@@ -14,6 +17,22 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onBlur={(e) => setAnimal(e.target.value)}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option value="">Please select an animal.</option>
+            {animals.map((a) => (
+              <option key={a} value={a}>
+                {a}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
