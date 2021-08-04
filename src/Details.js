04bfcc86d022c7b51702f908582d9a5fd677130b
yashimwong <yashimwong@gmail.com>
2021-08-04 23:13:39 +0800

    feat: add carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,10 @@
 import { Component } from "react";
 import { withRouter } from "react-router";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = { loading: true };
+
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -23,9 +22,16 @@ class Details extends Component {
   }
 
   render() {
-    const { animal, breed, city, state, description, name } = this.state;
+    if (this.state.loading) {
+      return <h2>Loading</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
